72af9185945189e1251e5406159bbcc7a9a599c6
lostone <lostone@noemail.net>
2019-03-29 18:47:03 +0000

    Bot Supervisor starts the Registry Poller S and Matcher supervisor.  proceeding to start bots from the configuration
    FossilOrigin-Name: 1176170b932a70bbf492b203b574bbd48ad2bd0aa8e4a9932cd28b4d569f8c2f

--- lib/nadia/governor/poller_supervisor.ex
+++ lib/nadia/governor/poller_supervisor.ex
@@ -1,20 +1,20 @@
-defmodule Nadia.BotSupervisor do
+defmodule Nadia.Supervisor.Poller do
   @moduledoc  """
-    Bot Supervisor
+    Bot Poller Supervisor
   """
   use Supervisor
 
   def start_link(bots) do
-    Supervisor.start_link(__MODULE__,bots)
+    Supervisor.start_link(__MODULE__,bots, name: __MODULE__)
   end
 
   def init(bots) do
-
     children  =   Enum.map(bots,fn (bot) ->
       #:crypto.strong_rand_bytes(5) |> Base.encode32
-      Supervisor.child_spec({Nadia.Poller, bot}, id: bot.bot_name )
+      Supervisor.child_spec({Nadia.Governor.Poller, bot}, id: bot.bot_name )
     end)
 
     Supervisor.init(children, strategy: :one_for_one)
   end
+
 end
